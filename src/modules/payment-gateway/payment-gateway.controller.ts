9471a88205b8f42d7073580dbd1b44f2bebbb6eb
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2026-08-01 14:32:49 +0100

    Added webhook endpoint

--- src/modules/payment-gateway/payment-gateway.controller.ts
+++ src/modules/payment-gateway/payment-gateway.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Param, Post, Req } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { PaymentProvider } from '../payment/enums/payment-provider.enum';
 import { PaymentGatewayService } from './payment-gateway.service';
 
@@ -6,12 +7,14 @@ import { PaymentGatewayService } from './payment-gateway.service';
 export class PaymentGatewayController {
   constructor(private readonly gatewayService: PaymentGatewayService) {}
   @Post('webhook/:provider')
+  @ApiOperation({})
   async handleWebhook(
     @Param('provider') provider: PaymentProvider,
     @Req() req: Request,
   ) {
     const response = await this.gatewayService.handleWebhook(provider, req);
 
+    console.log('response:', response);
     return response;
   }
 }
