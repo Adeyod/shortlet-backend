62b28e95f6096d33dfdd4b80254ee29b9e9cdbb5
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2026-08-04 15:48:50 +0100

    Added event listener to process addition of userId to bookings of the user after email verification

--- src/modules/notification/notification.module.ts
+++ src/modules/notification/notification.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BookingModule } from '../booking/booking.module';
+import { NotificationListener } from './listeners/notification.listener';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
 
 @Module({
+  imports: [BookingModule],
   controllers: [NotificationController],
-  providers: [NotificationService]
+  providers: [NotificationService, NotificationListener],
 })
 export class NotificationModule {}
